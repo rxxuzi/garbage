f753bbb5a3ac750a6d3d1e94fe6a8e4a498327e6
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-07-01 23:19:38 +0900

    garbage

--- example.py
+++ example.py
@@ -1,5 +1,12 @@
 import math
+
 for i in range(2, 11):
-    # Rounded down to the second decimal place
-    nishiyama = round(math.log(2, i), 2)
-    print(f"{i} = {nishiyama}")
+    log2 = math.log(i, 2)
+    nishiyama = round(log2)
+    print(f"log2({i}) = {nishiyama}")
+
+function = input("function: ")
+
+def f(x):
+    return x * x
+
---
+++ example1/complex.py
@@ -0,0 +1,13 @@
+x = 1 + 1j
+y = 1 + 2j
+z = 2 + 2j
+
+print(x + y)
+print(y + z)
+print(x * x / x)
+print(x ** 2)
+
+def f(x):
+    return abs(x)
+
+print(f(z))
